20a5b5115bf9fc5948168ca55bc746d79d79eeb2
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 13:39:09 -0300

    Criação das propriedades de times

--- Sessao2Api/Sessao2Api/Models/Times.cs
+++ Sessao2Api/Sessao2Api/Models/Times.cs
@@ -7,5 +7,8 @@ namespace Sessao2Api.Models
 {
     public class Times
     {
+        public int Cod_time { get; set; }
+        public string Uf_time { get; set; }
+        public string Nom_time { get; set; }
     }
 }
