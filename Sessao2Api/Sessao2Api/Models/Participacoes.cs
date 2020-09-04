286ca4baa76b21370cdd25ce08526c9ff791203e
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 14:47:02 -0300

    Criação do projeto para gerar uma api portátil

--- Sessao2Api/Sessao2Api/Models/Participacoes.cs
+++ Sessao2Api/Sessao2Api/Models/Participacoes.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ServiceProcess;
 
 namespace Sessao2Api.Models
 {
-    public class Participações
+    public class Participacoes
     {
         public int Cod_camp { get; set; }
         public int Cod_time { get; set; }
