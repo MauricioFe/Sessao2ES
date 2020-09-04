ab1e475d62b78a0919583720339bd7331b8602c6
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 13:27:43 -0300

    Criação das propriedades de estadio

--- Sessao2Api/Sessao2Api/Models/Estadios.cs
+++ Sessao2Api/Sessao2Api/Models/Estadios.cs
@@ -7,5 +7,9 @@ namespace Sessao2Api.Models
 {
     public class Estadios
     {
+        public int Cod_est { get; set; }
+        public string Nom_est { get; set; }
+        public int Capacidade { get; set; }
+        public string Uf_Estadio { get; set; }
     }
 }
