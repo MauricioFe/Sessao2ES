1bd160e945e4e4b3b5d0a30419cea652040cc575
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 13:24:23 -0300

    Adição da api

---
+++ Sessao2Api/Sessao2Api/Models/Patrocinios.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sessao2Api.Models
+{
+    public class Patrocinios
+    {
+    }
+}
