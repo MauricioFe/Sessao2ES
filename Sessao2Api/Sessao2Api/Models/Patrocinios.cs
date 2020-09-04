40171343d3c65c293f89f506df4765c88b648ecb
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 13:36:56 -0300

    Criação da propriedade de patrocinios

--- Sessao2Api/Sessao2Api/Models/Patrocinios.cs
+++ Sessao2Api/Sessao2Api/Models/Patrocinios.cs
@@ -7,5 +7,10 @@ namespace Sessao2Api.Models
 {
     public class Patrocinios
     {
+        public int Ano { get; set; }
+        public int Cod_time { get; set; }
+        public int Cod_pat { get; set; }
+        public decimal Valor { get; set; }
+
     }
 }
