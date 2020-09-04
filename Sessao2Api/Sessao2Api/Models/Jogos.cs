51e38bcbe6f44deca98e02773d280aa4971c64f3
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 13:32:48 -0300

    Criação das propriedades de jogos

--- Sessao2Api/Sessao2Api/Models/Jogos.cs
+++ Sessao2Api/Sessao2Api/Models/Jogos.cs
@@ -7,5 +7,11 @@ namespace Sessao2Api.Models
 {
     public class Jogos
     {
+        public int Cod_camp { get; set; }
+        public int Cod_time1 { get; set; }
+        public int Cod_time2 { get; set; }
+        public int Cod_estadio { get; set; }
+        public DateTime Data { get; set; }
+        public int resultado { get; set; }
     }
 }
