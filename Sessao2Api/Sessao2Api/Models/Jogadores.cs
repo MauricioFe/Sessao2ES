6793abf34212c4173faaa8d66ad7a148272680ca
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-04 13:31:21 -0300

    Criação das propriedades de jogadores

--- Sessao2Api/Sessao2Api/Models/Jogadores.cs
+++ Sessao2Api/Sessao2Api/Models/Jogadores.cs
@@ -7,5 +7,11 @@ namespace Sessao2Api.Models
 {
     public class jogadores
     {
+        public int Cod_jog { get; set; }
+        public DateTime Dat_nasc { get; set; }
+        public decimal Salario { get; set; }
+        public int  Cod_pos { get; set; }
+        public string Nom_jog { get; set; }
+        public int Cod_time { get; set; }
     }
 }
