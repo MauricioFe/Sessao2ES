3355cd0e72a4b40ad4a09369c2808c515f182608
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-08 14:39:10 -0300

    Criação da interface de jogadores e jogos

---
+++ Sessao2Api/Sessao2Api/Data/ICampeonatosDAL.cs
@@ -0,0 +1,16 @@
+﻿using Sessao2Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sessao2Api.Data
+{
+    public interface IJogosDAL
+    {
+        IEnumerable<Jogos> GetAll();
+        void Add(Jogos jogos);
+        void Update(Jogos jogos);
+        void Remove(int id);
+    }
+}
