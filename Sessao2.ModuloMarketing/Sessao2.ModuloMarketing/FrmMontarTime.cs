c6aaeebd93076ead540858b5edc6e25f7c75d50e
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-11 11:42:23 -0300

    Adicionar montar time

---
+++ Sessao2.ModuloMarketing/Sessao2.ModuloMarketing/FrmMontarTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sessao2.ModuloMarketing
+{
+    public partial class FrmMontarTime : Form
+    {
+        public FrmMontarTime()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMontarTime_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
