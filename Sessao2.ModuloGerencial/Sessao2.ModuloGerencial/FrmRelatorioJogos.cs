d224350be5856aecad019791c819ec434055b39f
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-16 13:39:05 -0300

    Consumindo api

--- Sessao2.ModuloGerencial/Sessao2.ModuloGerencial/FrmRelatorioJogos.cs
+++ Sessao2.ModuloGerencial/Sessao2.ModuloGerencial/FrmRelatorioJogos.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Sessao2.ModuloGerencial.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace Sessao2.ModuloGerencial
@@ -16,10 +19,21 @@ namespace Sessao2.ModuloGerencial
         {
             InitializeComponent();
         }
-
+        string URI = "http://localhost:5005/wstowers/api/jogos";
         private void FrmRelatorioJogos_Load(object sender, EventArgs e)
         {
 
+        }
+        public async void GetJogosMenorSalarioGanhador()
+        {
+            List<Jogos> jogosList = new List<Jogos>();
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{URI}/GetJogosMenorFolhaSalarialVenceu");
+                var jogos = await response.Content.ReadAsStringAsync();
+                jogosList = new JavaScriptSerializer().Deserialize<List<Jogos>>(jogos);
+
+            }
         }
     }
 }
---
+++ Sessao2.ModuloGerencial/Sessao2.ModuloGerencial/Models/Campeonato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sessao2.ModuloGerencial.Models
+{
+    class Campeonato
+    {
+        public int Cod_camp { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public string Tipo { get; set; }
+        public string DataInicio { get; set; }
+        public string DataFim { get; set; }
+        public string Def_tipo { get; set; }
+    }
+}
